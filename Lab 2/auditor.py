1508381eef84de492cf4e211d455214b762532a7
YJ <2603049@sit.singaporetech.edu.sg>
2026-09-22 06:16:48 +0800

    Lab 2

--- Lab 2/auditor.py
+++ Lab 2/auditor.py
@@ -1,7 +1,34 @@
 inventory = 0
+rejected = 0
 
 while True:
-    prompt = input("Enter stock quantity or type quit: ")
+    stock = input("Enter stock quantity or type quit: ")
 
-    if prompt == "quit":
+
+    if stock.lower() == "quit":
+        print("Total Units Processed:", inventory)
+        print("Number of Failed/Rejected Entries:", rejected)
+        break
+
+
+    if stock.startswith("-") and stock[1:].isdigit():
+        print("Error: Negative numbers are not allowed.")
+        rejected += 1
+        continue
+
+
+    elif not stock.isdigit():
+        print("Error: Please enter a valid integer.")
+        rejected += 1
+        continue
+
+
+    stock = int(stock)
+
+
+    inventory += stock
+
+
+    if inventory > 500:
+        print("OVERSTOCK ALERT!")
         break
